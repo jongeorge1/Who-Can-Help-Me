a940b4baaaa0254599f5e54079fadb4c67c0286b
jongeorge1 <jon@jonathangeorge.co.uk>
2010-09-24 22:09:13 +0100

    Tidied up batch files.

--- Common/AssemblyVersion.cs
+++ Common/AssemblyVersion.cs
@@ -13,7 +13,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
 
