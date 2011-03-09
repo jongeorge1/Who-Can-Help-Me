165ba61900233121492c562bc9296f514c3c8189
jongeorge1 <jon@jonathangeorge.co.uk>
2011-03-09 21:53:04 +0000

    Fix to TagByFirstCharactersOfName specification. CSS tweak. Removed invalid image file references.

--- Solutions/WhoCanHelpMe.Domain/Specifications/TagByFirstCharactersOfNameSpecification.cs
+++ Solutions/WhoCanHelpMe.Domain/Specifications/TagByFirstCharactersOfNameSpecification.cs
@@ -25,7 +25,10 @@
 
         public override Expression<Func<Tag, bool>> MatchingCriteria
         {
-            get { return t => t.Name.StartsWith(this.startingCharacters, StringComparison.CurrentCultureIgnoreCase); }
+            // HACK: Fugly hack here. The NHib Linq provider doesn't convert string.Substring)( correctly - the SQL substring 
+            // function it maps to is 1-based, wheras the .NET version is 0 based. So I've got to use 1 as the startIndex for 
+            // this to work properly.
+            get { return t => t.Name.Substring(1, this.startingCharacters.Length).ToLowerInvariant() == this.startingCharacters.ToLowerInvariant(); }
         }
     }
 }
